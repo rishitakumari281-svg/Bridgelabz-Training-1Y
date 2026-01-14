46be7b637ac2bbf924cb397ba394dfd6bb1c8caa
rishitakumari281-svg <rishitakumari281@gmail.com>
2026-01-14 11:31:33 +0530

    delete

--- java-primitive-user-input/Fee.java
+++
@@ -1,9 +0,0 @@
-public class Fee{
-	public static void main(String[] args){
-		double fee=125000.0;
-		double dis_per=10.0/100.0;
-		double amount=dis_per*fee;
-		double discount=fee-(amount);
-		System.out.println("The discount amount is INR "+amount+" and final discounted fee is INR "+discount);
-}}
-		
--- java-primitive-user-input/Profit.java
+++
@@ -1,9 +0,0 @@
-public class Profit{
-	public static void main(String[] args){
-		double cp=129, sp=191;
-		double profit=sp-cp;
-		double pp=(profit/cp)*100;
-		System.out.println("The cost price is INR "+cp+" and selling price is INR "+sp);
-		System.out.printf("\n The Profit is INR %.0f and profit percentage is %.2f ",profit,pp);
-	}
-}
