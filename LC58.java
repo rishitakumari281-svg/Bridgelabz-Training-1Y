ad3b13188359eab8714061f1ebc59601f9192768
rishitakumari281-svg <rishitakumari281@gmail.com>
2026-02-09 16:49:39 +0530

    [Rishita Kumari]add.classwork

---
+++ LC58.java
@@ -0,0 +1,7 @@
+class LC58 {
+    public int lengthOfLastWord(String s) {
+        String[] arr = s.split(" ");
+        String lastWord = arr[arr.length-1];
+        return lastWord.length();
+    }
+}
